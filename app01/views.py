89f1334fb35c7cb0fcfce0e39ffb5f6b4bf7133b
Dexter WU <1585829917@qq.com>
2022-03-20 15:38:41 +0800

    basic.html

--- app01/views.py
+++ app01/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, HttpResponse
 from django.conf import settings
 from utils.tencent.sms import send_sms_single
 
+
 # Create your views here.
 def send_sms(request):
     """ 发送短信 """
@@ -13,9 +14,37 @@ def send_sms(request):
     if not template_id:
         return HttpResponse("模板不存在")
 
-    code = random.randrange(1000,9999)
-    res = send_sms_single("18621503727", template_id, [code,])
+    code = random.randrange(1000, 9999)
+    res = send_sms_single("18621503727", template_id, [code, ])
     if res["result"] == 0:
         return HttpResponse("成功")
     else:
         return HttpResponse(res["errmsg"])
+
+
+from django import forms
+from app01 import models
+from django.core.validators import RegexValidator
+
+
+class RegisterModelForm(forms.ModelForm):
+    mobile_phone = forms.CharField(label="手机号", validators=[RegexValidator(r'^(1[3|4|5|6|7|8|9])\d{9}$', '手机号格式错误'), ])
+    password = forms.CharField(label="密码", widget=forms.PasswordInput())
+    comfirm_password = forms.CharField(label="重复密码", widget=forms.PasswordInput())
+    code = forms.CharField(label="验证码", widget=forms.TextInput())
+
+    class Meta:
+        model = models.UserInfo
+        # fields = "__all__"
+        fields = ['username', 'email', 'password', 'comfirm_password', 'mobile_phone', 'code']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():  # name是在数据库中的字段名
+            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["placeholder"] = "请输入%s" % (field.label,)
+
+
+def register(request):
+    form = RegisterModelForm()
+    return render(request, "app01/register.html", {"form": form})
