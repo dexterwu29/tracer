04d1de2493df4f51b3be0267a3499a891ee1e9f0
Dexter WU <1585829917@qq.com>
2022-03-20 16:29:40 +0800

    registration page display

--- web/views/account.py
+++ web/views/account.py
@@ -1,3 +1,10 @@
 """
 账户相关功能：注册、短信、登录、注销
 """
+from django.shortcuts import render
+from web.forms.account import RegisterModelForm
+
+
+def register(request):
+    form = RegisterModelForm()
+    return render(request, 'register.html', {"form": form})
